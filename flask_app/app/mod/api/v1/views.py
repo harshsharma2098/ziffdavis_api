fdacb0052f60eef61560ce0cd09262f702c74e12
adityak <aditya.k@genietalk.ai>
2021-05-26 15:37:52 +0530

    update

--- flask_app/app/mod/api/v1/views.py
+++ flask_app/app/mod/api/v1/views.py
@@ -110,7 +110,7 @@ def predict():
             elif "spell" in sen[i] or "enter" in sen[i] or "know" in sen[i]:
                 if "last and first" in sen[i] or "last name and first" in sen[i] or "last name first name" in sen[i] or "last in first name" in sen[i]:
                     temp["command"] = "DTMF_string"
-                    temp["value"] = f"{userdetails['lname']}"
+                    temp["value"] = f"{userdetails['lname']}{userdetails['fname']}"
                     response['action'].append(temp)
                     if "pound" in sen[i+1]:
                         temp = {}
