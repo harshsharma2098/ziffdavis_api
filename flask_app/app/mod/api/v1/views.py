73198f9841d72bfabb8c8ca6877e5049af28aac4
adityak <aditya.k@genietalk.ai>
2021-05-26 19:12:15 +0530

    update

--- flask_app/app/mod/api/v1/views.py
+++ flask_app/app/mod/api/v1/views.py
@@ -189,7 +189,7 @@ def predict():
                     temp["command"] = "DTMF" 
                     temp["value"] = strtoint.get(value[0])
                     response['action'].append(temp)
-            elif "transferring" in sen[i] or "wait while i transfer your call" in sen[i] or "being transferred please hold" in sen[i]:
+            elif "transferring" in sen[i] or ("wait while i transfer your call" in sen[i] and "operator" not in sen[i]) or "being transferred please hold" in sen[i]:
                 temp = {}
                 temp['command'] = "hangup"
                 temp['value'] = True
