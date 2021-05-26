95f617ae9070feb8b43fa929312aa14312f42c73
adityak <aditya.k@genietalk.ai>
2021-05-26 12:25:10 +0530

    update

--- flask_app/app/mod/api/v1/views.py
+++ flask_app/app/mod/api/v1/views.py
@@ -168,7 +168,7 @@ def predict():
                     temp["value"] = strtoint.get(value[0])
                     response['action'].append(temp)
             elif "dial by name" in sen[i]:
-                value = getno.findall(sen[i+1]) 
+                value = getno.findall(sen[i+1]) if i+1 > len(sen) else getno.findall(sen[i+1])
                 if value:
                     temp = {}
                     temp["command"] = "DTMF" 
